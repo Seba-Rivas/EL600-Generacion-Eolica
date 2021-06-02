734d549da3fb8d31a55d342a071bb6cb2448eaa7
Sebastián Rivas Vega <71355332+Seba-Rivas@users.noreply.github.com>
2021-06-01 20:30:49 -0400

    d

--- Costos/Proyeccion_costos.py
+++ Costos/Proyeccion_costos.py
@@ -2,3 +2,11 @@
 El presente script, tiene como objetivo proyuectar los costos marginales
 a utlizar en el proyecto de generación Eólica supuesto en el curso EL6000.
 '''
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Lo primero es estraer los datos de los archivos .tsv:
+
+df_PA = pd.read_csv(https://raw.githubusercontent.com/Seba-Rivas/EL600-Generacion-Eolica/main/Costos/Pan_de_Azucar_220kV.tsv, header=1)

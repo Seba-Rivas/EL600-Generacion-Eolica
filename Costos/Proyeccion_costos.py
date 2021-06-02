d1654e8cd5bcfce3736f211258fcbbb5ee28e48a
Sebastián Rivas Vega <71355332+Seba-Rivas@users.noreply.github.com>
2021-06-02 02:01:57 -0400

    inventando

--- Costos/Proyeccion_costos.py
+++ Costos/Proyeccion_costos.py
@@ -18,4 +18,42 @@ df_dolar = df_PA.iloc[:,[4]] # Apartamos al columna de costos en dolares}
 
 # Continuamos con el viento:
 url_wind = 'https://raw.githubusercontent.com/Seba-Rivas/EL600-Generacion-Eolica/main/recon_B6RS7S.csv'
-df_wind = pd.read_csv(url_wind, sep='[\s+,]' , engine='python')
+df_wind = pd.read_csv(url_wind, sep='[\s+,]' , engine='python', header = None)
+drp = np.arange(8781) # Eliminaremos todo el año 1980, pues no está completo
+drp  = drp.tolist()
+df_wind.drop(drp, axis=0, inplace=True)
+df_wind.index = range(len(df_wind))
+df_wind[0] = pd.to_datetime(df_wind[0])
+
+# Ahora, lo que debemos hacer es generar columnas con los años y filas
+#con las horas de cada día. No contemplaremos los días, pero luego debemo
+#hallar la manera de reintroducir los días si los necesitamos
+
+df = pd.DataFrame(columns= ['year', 'hour', 'velocity'])
+w_vel = []
+w_date = []
+w_hour = []
+
+for j in range(len(df_wind)):
+    data = pd.DataFrame()
+
+    if df_wind[0][j] == df_wind[0][0]:
+        w_vel.append(df_wind[2][j])
+        w_date.append(df_wind[0][j])
+        w_hour.append(df_wind[1][j])
+
+    if df_wind[0][j].year != df_wind[0][j-1].year or j == len(df_wind)-1:
+        data['date'] = w_date
+        data['hour'] = w_hour
+        data['velocity'] = w_vel
+        data.head()
+        df.append(data,ignore_index=True)
+
+        w_vel = []
+        w_date = []
+        w_hour = []
+
+    else:
+        w_vel.append(df_wind[2][j])
+        w_date.append(df_wind[0][j])
+        w_hour.append(df_wind[1][j])
